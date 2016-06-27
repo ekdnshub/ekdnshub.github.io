cefaf75518ed72a40c2b08824afd84acc64c0150
ekdnshub <ekdnshub@users.noreply.github.com>
2016-06-27 16:02:21 +0900

    Update postHandler.js

--- blog/scripts/postHandler.js
+++ blog/scripts/postHandler.js
@@ -84,19 +84,6 @@ var BlogLoadingBar = (function(){
 	}
 })();
 
-// 데이터 없는 포스트 처리
-try{
-	var currentSeq = $("#blog_seq").val();
-	var postMeta = postsMeta[currentSeq];
-
-	// 이곳에서 null 체크 된 것은 row db에 있는 것이다. 아니면 진짜 데이터가 날아갔거나...
-	if( postMeta == null ){
-		window.location.href="http://www.jdm.kr/404";
-	}
-} catch( ex ){
-	o.toast.error("parse Error.");
-}
-
 function applyContentEffect(){
 
 	// bootstrap class add
@@ -155,6 +142,17 @@ function applyContentEffect(){
 	Folder.init();
 };
 
+var addInfoBody = o.util.multiLine(function(){
+  /*!
+		<span id="content_category" class="text-muted" style="display:none;"></span>│<span id="content_created" class="mycolor1">2014-09-29</span>
+		<div class="btn-group" role="group">
+			<button id="bigsize" class="btn btn-default btn-xs"><i id="bigsizeLabel" class="fa fa-expand"> Wide</i></button>
+			<button id="up_font_size" class="btn btn-default btn-xs"><i class="fa fa-text-height"></i> 확대</button>
+			<button id="down_font_size" class="btn btn-default btn-xs"><i class="fa fa-text-height"></i> 축소</button>
+		</div>
+  */
+});
+$("#add_info").html(addInfoBody);
 
 // s: 추가 기능 버튼
 $("#bigsize").click(function(){
@@ -186,146 +184,6 @@ $("#down_font_size").click(function(){
 
 // s:우측 카테고리 관련
 
-// d3 chart print...
-if( !o.browser.isLowerIE(8) ){
-	o.loader.getScript( "//d3js.org/d3.v3.js", function(){
-		function printBarChart(data) {
-
-			var xName = "dateString";
-			var yName = "totalCount";
-
-			var id = "#count_d3";
-			$(id).text("");
-			var _width = $(id).width();
-			var _height = $(id).width(); // 정사각형이므로
-			var maxCountValue = D3maxValue(data,yName);
-
-			var margin = {
-				top : 20,
-				right : 0,
-				bottom : 30,
-				left : 0
-			};
-			var padding = 5; // 바 간격
-
-			var width = _width - margin.left - margin.right;
-			var height = _height - margin.top - margin.bottom;
-
-			var svg = d3.select(id).append("svg")
-				.attr("width",	width + margin.left + margin.right)
-				.attr("height", height + margin.top + margin.bottom).append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-			// 바 생성
-
-			svg.selectAll('rect')
-				.data(data)
-				.enter()
-				.append('rect')
-				.transition()
-				.attr('x', function (d, i) {
-					return i * ((width + padding) / data.length);
-				})
-				.attr('y', function (d) {
-					return height - ((d[yName])*(height/maxCountValue));
-				})
-				.attr('width', width / data.length - padding)
-				.attr('height', function (d) {
-					return (d[yName])*(height/maxCountValue);
-				});
-
-			// 바 라벨
-			svg.selectAll('g')
-				.data(data)
-				.enter()
-				.append('text')
-				.transition()
-				.text(function (d) {
-					return d[yName];
-				})
-				.attr('x', function (d, i) {
-					return i * ((width + padding) / data.length) + (width / data.length - padding) / 2;
-				})
-				.attr('y', function (d) {
-					return height - ((d[yName])*(height/maxCountValue)) + 10;
-				})
-				.attr('fill', function(d){
-					return '#31708f';
-				})
-				.attr('font-size', '0.75em')
-				.attr('text-anchor', 'middle');
-
-			// x축 표시
-			svg.selectAll('g')
-				.data(data)
-				.enter()
-				.append('text')
-				.transition()
-				.text(function (d) {
-					var formatData = d[xName]; // 2014-11-11
-					return formatData.replace("-","").substring(4,9);
-				})
-				.attr('x', function (d, i) {
-					return i * ((width + padding) / data.length) + (width / data.length - padding) / 2;
-				})
-				.attr('y', function (d) {
-					return height + 10;
-				})
-				.attr('font-size', '0.75em')
-				.attr('fill', function(d){
-					var date = new Date(d[xName]);
-					var today = new Date();
-					if( date.getDay() == 0 ) return "red";
-					if( date.getDay() == 6 ) return "blue";
-					if( date.getDay() == today.getDay() ) return "black";
-					return "gray";
-				})
-				.attr('text-anchor', 'middle');
-
-			o.ajax.send({
-				url:"//xapi.jdm.kr/counter/total",
-				type:"get",
-				done:function(data){
-					var totalCount = o.util.getCommaString( Number(data.tot) );
-					var x = $("#count_d3 svg").width();
-
-					// 우측 상단
-					svg.append('text')
-						.transition()
-						.text("현재까지 총 " + totalCount + "명 방문")
-						.attr('x', x)
-						.attr('y', -10)
-						.attr('text-anchor', 'end')
-						.attr('font-size', '0.75rem');
-					//.attr('fill', 'white');
-				}
-			});
-		}
-		function D3maxValue(data, attr){
-			var d = $(data);
-			var max = 0;
-
-			for( i = 0 ; i < d.length; i++ ){
-				if( Number(d[i][attr]) > max ){
-					max = Number(d[i][attr]);
-				}
-			}
-			return max;
-		}
-
-		o.ajax.send({
-			url:"//xapi.jdm.kr/counter/week",
-			type:"get",
-			done:function(data){
-				/*data = data.sort(function(a,b){ return b.dateString.replace(/-/g,"") - a.dateString.replace(/-/g, "") ; });*/
-				printBarChart(data);
-			}
-		});
-	});
-}else{
-	$("#count_d3").parent().hide();
-}
-
 // start : ReferencePost
 var referencePostTemplate = o.util.multiLine(function(){
 	/*!
@@ -346,8 +204,20 @@ function addReferencePost(item){
 	//$("#toplist_"+seq).hide().delay(500).fadeIn("slow");
 };
 
-function makeReferencePost(){
-	var seq = $("#blog_seq").val();
+function makeReferencePost(seq){
+  
+  var initBody = o.util.multiLine(function(){
+    /*!
+			<hr>
+			<h3 style="font-size:1.5em; font-weight:bold;">Reference Post</h3>
+			<a href="javascript:;" id="top_more_after"><span class="more text-center" style="border-bottom: 1px solid #eee;">▲more</span></a>
+			<ul class="list-unstyled" style="margin-bottom: 0px;">	
+			</ul>
+			<a href="javascript:;" id="top_more_before"><span class="more text-center" style="border-top: 1px solid #eee;">▼more</span></a>
+    */
+  });
+  $("#top_list").html(initBody);
+  
 	$("#top_list ul").html("");
 	var data = ArchiveGroup.findGroup(seq);
 	if( data == null ) return;
@@ -431,111 +301,6 @@ function showAnimationForReferenecePost( _obj, order, loopCnt, delay ){
 };
 // end : ReferencePost
 
-var SearchRegion = (function(){
-    function getSize(obj) {
-        var size = 0, key;
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) size++;
-        }
-        return size;
-    };
-
-    function go_search(){
-        var titleResults = {};
-        var bodyResults = {};
-        var searchStr = $("#search_str").val();
-        searchStr = searchStr.toLowerCase();
-
-        if( $.trim(searchStr) == "" || searchStr.length < 2 ){
-            o.toast.error("검색어는 최소 2자 이상 입력해주세요.");
-            return;
-        }
-
-        // search Title
-        $.each( postsInfo.getMeta(), function(i, obj){
-            if( obj.title.toLowerCase().indexOf(searchStr) >= 0 ){
-                titleResults[obj.title] = obj;
-            }
-        });
-
-        var childTemplate = o.util.multiLine(function(){
-            /*!
-                <li>
-                    <a href="/{{seq}}">
-                        <span style="display:block;">
-                        {{title}}
-                        <span class="pull-right hidden-xs"><span class="text-muted">{{category}}</span>│<span class="mycolor1">{{created}}</span></span>
-                        </span>
-                    </a>
-                </li>            
-            */
-        });
-
-        var template = o.util.multiLine(function(){
-            /*!
-                <div class="panel">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">" {{searchStr}} " 제목 검색 결과 총 <span class="mycolor1">{{titleCount}}</span>건</h3>
-                    </div>
-                    <div class="panel-body">
-                        <ul class="list-unstyled">
-                            {{titleChild}}
-                        </ul>
-                    </div>
-                </div>
-                <div class="panel">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">" {{searchStr}} " 본문 검색 결과 총 <span class="mycolor1">{{bodyCount}}</span>건</h3>
-                    </div>
-                    <div class="panel-body">
-                        <ul class="list-unstyled">
-                            {{bodyChild}}
-                        </ul>
-                    </div> 
-                </div>
-            */
-        });
-
-        var titleChild = "";
-        $.each(titleResults, function( name, obj ){
-        	obj.category = ArchiveGroup.findName( obj.seq );
-            titleChild += o.mapper.toHtml(childTemplate, obj);
-        });
-
-        var searchResult = {};
-        searchResult.searchStr = searchStr;
-        searchResult.titleCount = getSize(titleResults);
-        searchResult.titleChild = titleChild;
-        searchResult.bodyCount = 0;
-        searchResult.bodyChild = "업데이트 준비중입니다. ^^";
-        var html = o.mapper.toHtml(template, searchResult);
-
-        $("#left_wrap").html(html);
-
-        // css
-        $("#left_wrap .panel li a").css("text-decoration", "none");
-        $("#left_wrap .panel li").hover(
-            function(){ $(this).css("background", "#eee"); },
-            function(){ $(this).css("background", ""); }
-        );
-
-    };
-
-    return{
-        go_search:go_search
-    };
-
-})();
-
-$("#go_search").click(function(){
-    SearchRegion.go_search();
-});
-$("#search_str").keyup(function(event){
-    if( event.keyCode == '13' ){
-        SearchRegion.go_search();
-    }
-});
-
 var RightCategory = (function(){
 
 	var template = o.util.multiLine(function(){
@@ -568,7 +333,14 @@ var RightCategory = (function(){
 
 $( document ).ready(function(){
 	var maxPostsCnt = o.util.getObjectSize(postsInfo.getMeta()); // 포스트 총 개수
-	var currentSeq = $("#blog_seq").val(); // 현재 시퀀스
+	
+	/* current Seq 처리 */
+	var currentLocationToken = window.location.href.split("/");
+	var currentSeq = Number(currentLocationToken[currentLocationToken.length-1]);
+	
+  /* header 처리 */
+  Header.printGnb();
+  Header.orgTopSubject( "JDM's Blog", "/blog", "온갖 테스트 결과가 기록되는 이곳은 JDM's Blog입니다. :3");
 	
 	/* 아카이브 그룹 초기화 */
 	ArchiveGroup.init(maxPostsCnt);
@@ -580,7 +352,16 @@ $( document ).ready(function(){
 	RightCategory.init();
 	
 	/* 관련 게시글 목록 생성 */
-	makeReferencePost();
+	makeReferencePost(currentSeq);
+	
+	/* 코멘트 관련 처리 */
+	Comment.init(currentSeq);
+	
+	/* 카피라이트 문구 처리 */
+	Copyright.init();
+	
+	/* Bottom 처리(SideBar) */
+	Bottom.init();
 	
 	/* 포스트 총 개수 출력 */
 	$("#content_total").html(maxPostsCnt);
