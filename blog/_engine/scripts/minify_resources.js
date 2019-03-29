9b71a45b80ac6a96c062d4664827bc40d3a6bbcd
ekdnshub <ekdns7184@hanmail.net>
2019-03-29 21:31:08 +0900

    css 미니파이 시작

--- blog/_engine/scripts/minify_resources.js
+++ blog/_engine/scripts/minify_resources.js
@@ -4,12 +4,14 @@ https://github.com/srod/node-minify
 
 $ npm install @node-minify/core
 $ npm install @node-minify/google-closure-compiler
+$ npm install @node-minify/clean-css
 
-이 스크립트는 블로그에서 사용하는 모든 스크립트를 minify 하기 위한 용도로 제작 되었다.
+이 스크립트는 블로그에서 사용하는 모든 css/스크립트를 minify 하기 위한 용도로 제작 되었다.
 */
 
 const minify = require('@node-minify/core');
 const gcc = require('@node-minify/google-closure-compiler');
+const cleanCSS = require('@node-minify/clean-css');
 
 var resourcesMinify = (function() {
 
@@ -37,7 +39,20 @@ var resourcesMinify = (function() {
         }
 
         if (type == "all" || type == "view") {
-            console.log("블로그 본문용 리소스 미니파이 시작...");
+            console.log("블로그 본문용 css 미니파이 시작...");
+            minify({
+              compressor: cleanCSS,
+              input: [
+                  "../../css/view.css"
+              ],
+              output: "../../css/view.min.css",
+              callback: function(err, min) {
+                if (err) {
+                  console.log("error:", err);
+                }
+                  console.log("블로그 본문용 css 미니파이 완료!");
+              }
+            });
             minify({
               compressor: gcc,
               input: [
@@ -60,6 +75,20 @@ var resourcesMinify = (function() {
             });
         }
         if (type == "all" || type == "main") {
+            console.log("블로그 메인용 css 미니파이 시작...");
+            minify({
+              compressor: cleanCSS,
+              input: [
+                  "../resources/main/main.css"
+              ],
+              output: "../../css/main.min.css",
+              callback: function(err, min) {
+                if (err) {
+                  console.log("error:", err);
+                }
+                  console.log("블로그 메인용 css 미니파이 완료!");
+              }
+            });
             console.log("블로그 메인용 리소스 미니파이 시작...");
             minify({
               compressor: gcc,
